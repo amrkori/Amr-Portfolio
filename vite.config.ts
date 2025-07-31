c7405cf926ff1784e6c788da6362442d37bda1f7
Amr Mohamed <164408462+amrkori@users.noreply.github.com>
2025-07-31 13:48:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// ✅ غيّر الـ base ليطابق اسم الريبو على GitHub
 export default defineConfig({
+  base: '/Amr-Portfolio/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
